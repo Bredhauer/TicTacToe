bb7266375ecd5aebe06fad81c1aabac38594239b
bredhauer <bredgauer.e@gmail.com>
2025-03-20 20:46:36 +0100

    Created a new project

---
+++ src/TicTacToe.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+    int boardWidth = 600;
+    int boardHeight = 650; //50px for the text panel on top
+
+    JFrame frame = new JFrame();
+
+    TicTacToe() {
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
+}
